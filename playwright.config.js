075f6a4c499d4a98cd1c08ef9fab8aea127fb635
alok.kumar <alok.kumar@circles.co>
2024-07-29 02:00:33 +0530

    Refactored code

--- playwright.config.js
+++ playwright.config.js
@@ -138,7 +138,7 @@ const config = {
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
+  outputDir: 'allure-results/',
 
   /* Run your local dev server before starting the tests */
   // webServer: {
